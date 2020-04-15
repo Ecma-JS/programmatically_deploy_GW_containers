a01ad86228e85419605571ebb9da32afa49e2339
iramarkjs <ira@jspro.dev>
2020-04-15 13:39:42 +0300

    script

--- app.js
+++ app.js
@@ -12,29 +12,12 @@ router.use(function (req,res,next) {
 });
 
 router.get('/app', function(req,res){
-  console.log(dockerMachineName);
-  //res.send('Hello World!');
-  childProcess.exec('docker-machine create --driver virtualbox --virtualbox-cpu-count -1 '+ dockerMachineName, (err, stdout) => {
-    console.log(stdout);
-    const req = 'eval $(docker-machine env ' + dockerMachineName + ')';
-    console.log(req)
-    childProcess.exec(req,  (err, stdout, stderr) => {
-      childProcess.exec('docker-machine active', (err, stdout, stderr) => {
-        console.log(stdout)
-        console.log(stderr)
-        childProcess.exec('docker build -t irina/docker-nginx .', (err, stdout) => {
-          console.log(stdout);
-          childProcess.exec('docker run --publish=80:80 irina/docker-nginx', (err, stdout, stderr) => {
-            dockerMachineName++
-          });
-        });
-      })
-
-    });
-  });
-
-
   res.send(dockerMachineName.toString());
+
+  childProcess.exec('bash script ' + dockerMachineName);
+  
+  dockerMachineName++;
+  
 });
 
 app.use(express.static(path));
